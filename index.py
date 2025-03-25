ea899a216cda30658af370fa29b74ced9e87dc14
張睿浩 <ray949397@gmail.com>
2025-03-25 12:19:17 +0800

    v2

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	homepage = "<h1>張睿浩Python網頁</h1>"
+	homepage = "<h1>張睿浩Python網頁1</h1>"
 	homepage += "<a href=/mis>MIS</a><br>"
 	homepage += "<a href=/today>顯示日期時間</a><br>"
 	homepage += "<a href=/welcome?nick=RuiHao>傳送使用者暱稱</a><br>"
